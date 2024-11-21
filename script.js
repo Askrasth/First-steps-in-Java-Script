37f70449191b7781f5cd027885857c608d6f4f50
Askrasth <askrasth@gmail.com>
2024-11-21 09:57:39 -0300

    Add files via upload
    Hey guys These are my first sintaxes of relearning js and taking this path of being a web developer seriously. That's just the begining of my journey

---
+++ script.js
@@ -0,0 +1,44 @@
+alert('this is a js sintax');
+
+alert(' this is a java script page')
+
+document.getElementById('sec1').innerHTML="The <i>Java Script</i> is an interresting <b>Programing language</b> to learn";
+
+document.getElementById('sec2').innerHTML="We could say that, <i> Html, Css </i> and <i> Java script</i> are the <b> Big power trio </b> of <b> Front - end </b> web development.";
+
+
+
+console.log("this is another output of information with js")
+
+
+let age = 18
+if (age >= 18){
+console.log('you are an adult')
+}else{
+    console.log('you are an adolescent');
+}
+
+
+let note = 95
+
+if (note >= 60 ){
+    console.log('You are approved')
+}else{
+    console.log('You were rejected')
+}
+
+
+document.write('did i get it ?')
+
+alert('VICTOOOOOOOOOOOORY AND GLORY WE SHALL CELEBRATE')
+
+document.write(' Finally i learned how to use <i> document.write</i>! Now i know it was the letter <b> W </b> which was my challenge at this code line. The w must be <u> simple </u> and not as <u> capslock.</u>  ')
+
+document.write(' So, these are the four forms of outputting information: <u> alert, console.log, document.getElementById </u> and <u> documet.write</u>')
+
+document.write(' thats just the begining of my js journey.')
+
+document.write(' i liked this so much that im using this sintax a lot.')
+
+document.getElementById('sec3').innerHTML="Coding is not hard. Just face the challenges and overcome it. Each code line mastered and each challenge passed is a new level and a new step ahead. Lets go bro to the end on this journey.";
+
